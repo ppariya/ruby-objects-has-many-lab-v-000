bcdcddd4e33f0e190f4f35a54f690de084f9040e
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-04-28 05:43:19 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+  attr_accessor :name, :songs
+  @@song_count = 0
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
+  def add_song_by_name(title)
+    song = Song.new(title)
+    add_song(song)
+  end
+
+  def self.song_count
+    return @@song_count
+  end
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author != nil
+      @author.name
+    else
+      return nil
+    end
+  end
+end
