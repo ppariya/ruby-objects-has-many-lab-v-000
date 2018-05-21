112e4a51fc33347ca94d92afbac938c67628b381
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-21 12:21:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
     attr_accessor :name, :songs
+    @@song_count = 0
 
     def initialize(name)
       @name = name
@@ -9,6 +10,7 @@ class Artist
     def add_song(song)
       self.songs << song
       song.artist = self
+      @@song_count += 1
     end
 
     def add_song_by_name(title)
