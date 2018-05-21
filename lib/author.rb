db02fee98a36bc9c9a0c57ed2a5a4e9a7a3da422
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-05-21 12:33:24 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,14 @@
 class Author
+  attr_accessor :name, :posts
+
+  def initialize (name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    self.posts << post
+    post.name = self
+  end
 
 end
